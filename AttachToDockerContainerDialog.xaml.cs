3f4e9019b0d19a22b50196a1482bfc58fa75016b
Scott Liddick <sliddick@perfectserve.net>
2019-10-15 09:33:16 -0600

    PFC-57: Added ability to define docker dotnet process entrypoints in a json config file in <solution root>.  Added config load status messag in UI.  Updated vsix manifest.

--- AttachToDockerContainerDialog.xaml.cs
+++ AttachToDockerContainerDialog.xaml.cs
@@ -3,6 +3,9 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
+using System.IO;
+using System.Web.Script.Serialization;
+using EnvDTE;
 using Microsoft.VisualStudio.PlatformUI;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
@@ -14,19 +17,25 @@ namespace AttachToDockerContainer
         private const string VsDbgDefaultPath = "/vsdbg/vsdbg";
 
         private readonly IServiceProvider _serviceProvider;
+        private readonly AttachToDockerContainerConfig _config;
 
         public AttachToDockerContainerDialog(IServiceProvider serviceProvider)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
+            var dte = ((AttachToDockerContainerPackage)serviceProvider).GetServiceAsync(typeof(DTE)) as DTE;
+
             _serviceProvider = serviceProvider;
             InitializeComponent();
+
+            _config = GetConfig();
 
             AttachButton.IsEnabled = false;
             PidComboBox.IsEnabled = false;
 
             var containerNames = GetContainerNames();
-            var (previousContainer, previousVsDbgPath) = GetSettings();
+            var processNames = GetProcessNames();
+            var (previousContainer, previousVsDbgPath, previousProcessName) = GetSettings();
 
             ContainerComboBox.ItemsSource = containerNames;
             ContainerComboBox.Text = containerNames.Contains(previousContainer)
@@ -35,9 +44,30 @@ namespace AttachToDockerContainer
 
             VsDbgPathTextBox.Text = previousVsDbgPath ?? VsDbgDefaultPath;
 
+            if (processNames.Any())
+            {
+                ProcessNameComboBox.IsEnabled = true;
+                ProcessNameComboBox.ItemsSource = processNames;
+                ProcessNameComboBox.Text = processNames.Contains(previousProcessName)
+                    ? previousProcessName
+                    : processNames.FirstOrDefault();
+            }
+            else
+            {
+                ProcessNameComboBox.IsEnabled = false;
+                ProcessNameComboBox.ItemsSource = new[] { "" };
+                ProcessNameComboBox.SelectedIndex = 0;
+            }
+
             UpdateDotNetPIDs();
 
             ContainerComboBox.SelectionChanged += ContainerComboBox_SelectionChanged;
+            ProcessNameComboBox.SelectionChanged += ProcessNameTextBox_SelectionChanged;
+        }
+
+        private void ProcessNameTextBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateDotNetPIDs();
         }
 
         private void AttachButton_Click(object sender, RoutedEventArgs e)
@@ -46,9 +76,10 @@ namespace AttachToDockerContainer
 
             var containerName = ContainerComboBox.Text;
             var vsDbgPath = VsDbgPathTextBox.Text;
+            var processName = ProcessNameComboBox.Text;
             var pid = (int)PidComboBox.SelectedItem;
 
-            SetSettings(containerName, vsDbgPath);
+            SetSettings(containerName, vsDbgPath, processName);
 
             DebugAdapterHostLauncher.Instance.Launch(containerName, vsDbgPath, pid);
             Close();
@@ -56,8 +87,7 @@ namespace AttachToDockerContainer
 
         private void ContainerComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Action action = () => UpdateDotNetPIDs();
-            Dispatcher.BeginInvoke(action);
+            UpdateDotNetPIDs();
         }
 
         private void UpdateDotNetPIDs()
@@ -69,7 +99,11 @@ namespace AttachToDockerContainer
             if (string.IsNullOrWhiteSpace(containerName))
                 return;
 
-            var pidofResult = DockerCli.Execute($"exec -it {containerName} pidof dotnet");
+            var processName = ProcessNameComboBox.SelectedItem as string;
+            if (string.IsNullOrWhiteSpace(processName))
+                return;
+
+            var pidofResult = DockerCli.Execute($"exec -it {containerName} pidof {processName}");
 
             var dotnetPidsParsed = pidofResult
                 .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
@@ -106,7 +140,46 @@ namespace AttachToDockerContainer
                 .ToArray();
         }
 
-        private (string container, string vsDbg) GetSettings()
+        private string[] GetProcessNames()
+        {
+            return _config.DebuggableProcessNames;
+        }
+
+        private AttachToDockerContainerConfig GetConfig()
+        {
+            StatusMessage.Text = string.Empty;
+            var config = new AttachToDockerContainerConfig();
+            try
+            {
+                ThreadHelper.ThrowIfNotOnUIThread();
+                var dte = (DTE)_serviceProvider.GetService(typeof(DTE));
+                var solutionRoot = Path.GetDirectoryName(dte.Solution.FileName);
+
+                var configFile = Path.Combine(solutionRoot, "container-debug-config.json");
+                using (var sr = new StreamReader(configFile))
+                {
+                    string jsonString = sr.ReadToEnd();
+                    JavaScriptSerializer ser = new JavaScriptSerializer();
+                    config = ser.Deserialize<AttachToDockerContainerConfig>(jsonString);
+                }
+                if (!config.DebuggableProcessNames.Any())
+                {
+                    throw new Exception("Missing or empty DebuggableProcessNames.");
+                }
+                StatusMessage.Foreground = System.Windows.Media.Brushes.DarkGreen;
+                StatusMessage.Text = "Configuration successfully loaded.";
+                return config;
+            }
+            catch (Exception ex)
+            {
+                StatusMessage.Foreground = System.Windows.Media.Brushes.DarkRed;
+                StatusMessage.Text = $"Problem loading <solution root>\\container-debug-config.json: {ex.Message}";
+            }
+
+            return config;
+        }
+
+        private (string container, string vsDbg, string processname) GetSettings()
         {
             const string collectionPath = nameof(AttachToDockerContainerDialog);
 
@@ -120,11 +193,12 @@ namespace AttachToDockerContainer
 
             SettingsStore.GetString(collectionPath, "container", out string container);
             SettingsStore.GetString(collectionPath, "vsdbg", out string vsdbg);
+            SettingsStore.GetString(collectionPath, "processname", out string processname);
 
-            return (container, vsdbg);
+            return (container, vsdbg, processname);
         }
 
-        private void SetSettings(string container, string vsdbg)
+        private void SetSettings(string container, string vsdbg, string processname)
         {
             const string collectionPath = nameof(AttachToDockerContainerDialog);
 
@@ -138,6 +212,7 @@ namespace AttachToDockerContainer
 
             SettingsStore.SetString(collectionPath, "container", container);
             SettingsStore.SetString(collectionPath, "vsdbg", vsdbg);
+            SettingsStore.SetString(collectionPath, "processname", processname);
         }
 
         private IVsWritableSettingsStore _settingsStore = null;
@@ -157,6 +232,16 @@ namespace AttachToDockerContainer
                 }
                 return _settingsStore;
             }
+        }
+
+        public class AttachToDockerContainerConfig
+        {
+            public AttachToDockerContainerConfig()
+            {
+                DebuggableProcessNames = Array.Empty<string>();
+            }
+
+            public string[] DebuggableProcessNames { get; set; }
         }
     }
 }
